761d9565ec1ce37ec3aabe09c4a41bdbb51bc6f0
SonGoku130 <aaryan2221t@gmail.com>
2026-06-08 17:07:47 +0530

    Add files via upload

---
+++ HAPPYBIRTHDAYDANIEL.py
@@ -0,0 +1,3 @@
+name = input("Enter your name = ")
+birthday = input("Enter your birthday = ")
+print("Sooooo your birthday is on", birthday, "Well happy birthday in advance", name, "or just happy birthday if today is your birthday cuz i dont rly know what you typed :p") 
