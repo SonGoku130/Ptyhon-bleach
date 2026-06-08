9176b3dd314876990c19a2afef751d3201e3f4a2
SonGoku130 <aaryan2221t@gmail.com>
2026-06-08 16:34:36 +0530

    Add files via upload

---
+++ practoce.py
@@ -0,0 +1,22 @@
+#printing a statement
+print("Watashino", "Soul society", "yare yare")
+
+#Printing a number
+print("Hado no kokyu", 99)
+
+a1=99
+A1=2
+_a1=2
+
+#Printing a calculation
+print(1*6+7)
+
+print("Number", 72)
+
+#Pass multiple arguments for printing or something idk
+print("Hello no.", 5, end='   !   ', sep='-')
+
+#for the end
+print('Hollow knight', end='!')
+
+print("The knight")
