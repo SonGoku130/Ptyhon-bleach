ca67c2b583765a8a0a363b8ea31295663ef830c4
SonGoku130 <aaryan2221t@gmail.com>
2026-06-08 16:45:23 +0530

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,12 @@
+#Variable creation
+x = "Kaiser"
+y = "Michael"
+print(x)
+print(y)
+
+aizen = "Aizen"
+print(aizen)
+
+#Input
+name = input("Enter your name =")
+print("\nHello", name, "\nWelcome to blue lock!")
